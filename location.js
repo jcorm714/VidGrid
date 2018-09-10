54dbaad6e2781697d6c0b87248908994c19c3008
Josh <Josh@DESKTOP-QM68RKH>
2018-09-10 03:20:19 -0400

    Added some stuffs

--- location.js
+++ location.js
@@ -1,27 +1,27 @@
-let canvas = document.getElementById("c1");
+let can = document.getElementById("c1");
 
-var locations = [
-  "area1":{"x":canvas.width/4,"y":canvas.height/4;},
-  "area2":{"x":canvas.width/2,"y":canvas.height/4;},
-  "area3":{"x":canvas.width * (3.0/4.0),"y":canvas.height/4;},
-  "area4":{"x":canvas.width,"y":canvas.height/4;},
-  "area5":{"x":canvas.width/4, "y": canvas.height/2},
-  "area6":{"x":canvas.width/2, "y": canvas.height/2},
-  "area7":{"x":canvas.width * (3.0/4.0), "y": canvas.height/2},
-  "area8":{"x":canvas.width, "y": canvas.height/2},
-  "area9":{"x":canvas.width/4, "y": canvas.height*(3.0/4.0)},
-  "area10":{"x":canvas.width/2, "y": canvas.height*(3.0/4.0)},
-  "area11":{"x":canvas.width *(3.0/4.0), "y": canvas.height*(3.0/4.0)},
-  "area12":{"x":canvas.width, "y": canvas.height*(3.0/4.0)},
-  "area13":{"x":canvas.width/4, "y": canvas.height},
-  "area14":{"x":canvas.width/2, "y": canvas.height},
-  "area15":{"x":canvas.width*(3.0/4,0), "y": canvas.height},
-  "area16":{"x":canvas.width, "y": canvas.height}
-];
+var locations = {
+  "area1":{"x":can.width/4,"y":can.height/4},
+  "area2":{"x":can.width/2,"y":can.height/4},
+  "area3":{"x":can.width * (3.0/4.0),"y":can.height/4},
+  "area4":{"x":can.width,"y":can.height/4},
+  "area5":{"x":can.width/4, "y": can.height/2},
+  "area6":{"x":can.width/2, "y": can.height/2},
+  "area7":{"x":can.width * (3.0/4.0), "y": can.height/2},
+  "area8":{"x":can.width, "y": can.height/2},
+  "area9":{"x":can.width/4, "y": can.height*(3.0/4.0)},
+  "area10":{"x":can.width/2, "y": can.height*(3.0/4.0)},
+  "area11":{"x":can.width *(3.0/4.0), "y": can.height*(3.0/4.0)},
+  "area12":{"x":can.width, "y": can.height*(3.0/4.0)},
+  "area13":{"x":can.width/4, "y": can.height},
+  "area14":{"x":can.width/2, "y": can.height},
+  "area15":{"x":can.width*(3.0/4,0), "y": can.height},
+  "area16":{"x":can.width, "y": can.height}
+};
 
 function picLocation(x, y)
 {
-  for key in locations
+  for(key in locations)
   {
     if(x<key.x && y <key.y)
         return key;
--- script.js
+++ script.js
@@ -2,7 +2,9 @@ let canvas = document.getElementById('c1');
 let ctx = canvas.getContext('2d');
 let video = document.getElementById('video');
 let button = document.querySelector("button");
-
+let order = Array(16).fill(0,0,16);
+order.foreach()
+console.log(order);
 $(function() {
   video.addEventListener('play', function() {
     let $this = this;
@@ -14,7 +16,7 @@ $(function() {
         {
           drawLineVert("rgba(255,255,255,1)", canvas.width - canvas.width/4 * i);
           drawLineHorz("rgba(255,255,255,1)", canvas.height- canvas.height/4 * i);
-          
+
         }
         setTimeout(loop, 1000 / 30);
       }
@@ -27,7 +29,9 @@ button.addEventListener("click", function(){
 } );
 
 
-
+///<summary>draws a line vertically across a canvas</summary>
+///<param name = "color">the color which the line is</param>
+///<param name = "pos"> the position to which the line starts</param>
 function drawLineVert(color, pos)
 {
   ctx.save();
@@ -38,6 +42,10 @@ function drawLineVert(color, pos)
   ctx.stroke();
   ctx.restore();
 }
+
+///<summary>draws a line horizontally across a canvas</summary>
+///<param name = "color">the color which the line is</param>
+///<param name = "pos"> the position to which the line starts</param>
 function drawLineHorz(color, pos)
 {
   ctx.save();
@@ -48,8 +56,18 @@ function drawLineHorz(color, pos)
   ctx.stroke();
   ctx.restore();
 }
+
+
+///<summary>Returns a canvas which can be used draw an image onto another</summary>
+///<returns>canvas</returns>
+///<param name = "img">an image to grab a region of </param>
+///<param name = "x">the x coordinate to start at for the image</param>
+///<param name = "y">the y coordinate to start at for the image</param>
+///<param name = "height">the distance the crop covers vertically</param>
+///<param name = "width">the distance the crop covers vertically</param>
 function getRegion(img, x, y, height, width)
 {
+
   let canvas2 = document.getElementById("c2");
   let ctx2 = canvas.getContext("3d");
   canvas2.width = width;
@@ -57,4 +75,8 @@ function getRegion(img, x, y, height, width)
 
   ctx.drawImage(img, x, y, width, height, 0 , 0, width, height);
   return canvas2;
+}
+function randomNum(max)
+{
+    return Math.floor(Math.random()*max) + 1;
 }
